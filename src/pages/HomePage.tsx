8970074d52837694666bdf4bd2ba26814b08ab97
jiwon <89182250+YooJiwon-github@users.noreply.github.com>
2024-10-04 10:46:03 +0900

    [chore] 코드 정리

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
-import TimePicker from '../components/timeSetModal/TimePicker';
 
 const HomePage: React.FC = () => {
   return (
     <div className=' w-full h-full'>
-      <TimePicker />
-      {/* <Navbar /> */}
+      <Navbar />
     </div>
   );
 };
