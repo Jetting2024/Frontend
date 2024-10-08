ae35ab17f59aa3d973a4ad319c3bc0fec4bf11db
jiwon <89182250+YooJiwon-github@users.noreply.github.com>
2024-10-08 20:08:28 +0900

    [design] 메인 화면 퍼블리싱 완료

---
+++ src/components/travellist/TravelList.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TravelList: React.FC = () => {
+  return (
+    <button className="min-w-[250px] min-h-[300px] border border-[#3d3d3d] border-opacity-30 shadow-sm rounded-md hover:bg-lightgray transform transition-transform duration-300 ease-out hover:-translate-y-2"></button>
+  );
+};
+
+export default TravelList;
