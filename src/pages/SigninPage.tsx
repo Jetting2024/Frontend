56a5282795ccc039b1af5becafe994e7d379a028
jiwon <jiwon0768@tukorea.ac.kr>
2024-12-30 19:00:28 +0900

    [chore] 오류 수정

--- src/pages/SigninPage.tsx
+++ src/pages/SigninPage.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { SiKakaotalk } from "react-icons/si";
 import teamworkImage from "../assets/teamwork.png";
+import { useSetRecoilState } from "recoil";
+import { authState } from "../global/recoil/authAtoms";
 import { KAKAO_AUTH_URL } from "../auth/OAuth";
 
 const SigninPage: React.FC = () => {
@@ -13,42 +15,8 @@ const SigninPage: React.FC = () => {
 
   const setAuth = useSetRecoilState(authState);
 
-  useEffect(() => {
-    const code = new URL(window.location.href).searchParams.get("code");
-    if (code) {
-      // 백엔드에 인가 코드를 보내고 accessToken, refreshToken을 받음
-      axios
-        .get(`http://localhost:8080/member/kakao/callback?code=${code}`)
-        .then((response) => {
-          const { accessToken, refreshToken } = response.data.data; // TokenResponseDto에서 data 추출
-
-          // 로컬 스토리지에 토큰 저장
-          localStorage.setItem("accessToken", accessToken);
-          localStorage.setItem("refreshToken", refreshToken);
-
-          // Recoil 상태 설정 (로그인 상태로 설정)
-          setAuth({ isAuthenticated: true, accessToken, refreshToken });
-
-          // 토큰이 잘 저장되었는지 확인
-          console.log("accessToken:", localStorage.getItem("accessToken"));
-          console.log("refreshToken:", localStorage.getItem("refreshToken"));
-
-          // 홈 화면으로 리디렉션
-          navigate("/");
-        })
-        .catch((error) => {
-          console.error("카카오 로그인 실패:", error);
-        });
-    }
-  }, [navigate, setAuth]);
-
   const KakaoLogin = async () => {
-    // window.location.href = "http://localhost:8080/member/kakao";
-    window.open(
-      "http://localhost:8080/member/kakao",
-      "_blank",
-      "width=500,height=700"
-    );
+    navigate("/member/kakao/callback");
   };
 
   const LoginClick = () => {
